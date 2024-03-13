21e93022bd8cb679b31a3410c6324ee667b4ada8
KevinRinconN <estebanrincon0926@gmail.com>
2024-03-13 13:18:05 -0500

    add page login and shadcn components

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,38 @@
+import { UserAuthForm } from "@/components/auth"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import Link from "next/link"
+
+export default function LoginPage(){
+    
+    return (
+        <Card className="max-w-sm">
+
+          <CardHeader>
+            <CardTitle className="text-2xl">Sign in</CardTitle>
+            <CardDescription>Enter your email below to create your account</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <UserAuthForm/>
+          </CardContent>
+          <CardFooter className="grid">
+            <p className="px-8 text-center text-sm text-muted-foreground">
+                Don't have an account?
+                <Link 
+                    className="ml-2 text-primary hover:underline underline-offset-4 hover:text-primary-accent"
+                    href={'new-account'}
+                >
+                    Sign up
+                </Link>
+            </p>
+            
+          </CardFooter>
+        </Card>
+ )
+}
