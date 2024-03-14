bde9d76035ed5b7c8fa6fed9f074781cb0933aa4
gustavo <gustavo.perez.231191@gmail.com>
2024-03-13 21:06:57 -0600

    landing page v.0.1 in progress

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+const NavBar = () => {
+    return (
+        <header className="px-8 py-4 bg-black flex justify-between items-center sticky top-0 z-10">
+            <Image src="/LOGOBLANCO.png" alt="logo" width={128} height={64} />
+            <nav>
+                <ul className="flex gap-4 text-white">
+                    <Link href="/register">
+                        <li>Register</li>
+                    </Link>
+                    <Link href="/login">
+                        <li>Sign In</li>
+                    </Link>
+                    {/*
+                    Final section to add team info 
+                    <Link href="/register">
+                        <li>About</li>
+                    </Link>
+                     */}
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default NavBar;
